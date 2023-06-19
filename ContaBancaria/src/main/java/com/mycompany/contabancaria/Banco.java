c320f79528183022d1c07d349c343903b9c92d84
moacir90 <123340944+moacir90@users.noreply.github.com>
2023-06-19 11:59:55 -0300

    Add files via upload

---
+++ ContaBancaria/src/main/java/com/mycompany/contabancaria/Banco.java
@@ -0,0 +1,14 @@
+package com.mycompany.contabancaria;
+
+
+public class Banco {
+
+    
+    public static void main(String[] args) {
+        
+        Conta contaBradesco = new Conta();
+        contaBradesco.mostraSaldo();
+        
+        
+    }
+}
---
+++ ContaBancaria/src/main/java/com/mycompany/contabancaria/Conta.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.contabancaria;
+
+
+public class Conta {
+    
+//Propriadades
+    double saldo;
+            
+    
+//Metodos
+//a) Construtor(deve possuir entrada de valores)
+    public Conta(){
+    
+    }
+//b) mostraSaldo(void que nao deve possuir entrada de valor)
+    
+    public void mostraSaldo(){
+        System.out.println("Seu saldo e"+saldo);
+    
+    }
+    
+//c) deposito(void que possuir entrada de valor)
+    
+//d) saque(void que possui entra de valor e atualiza o saldo)
+    
+    
+}
+
+//
